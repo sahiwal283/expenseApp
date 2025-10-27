88221e8af87e98666c3a062fed56dcb8e94a0290
sahiwal283 <62818451+sahiwal283@users.noreply.github.com>
2025-10-27 14:13:35 -0400

    refactor(phase3): Add index files for extracted components
    Created centralized exports:
    - src/components/expenses/ExpenseModal/index.ts
    - src/components/expenses/ExpenseTable/index.ts
    
    Simplifies imports in main orchestrator file

---
+++ src/components/expenses/ExpenseModal/index.ts
@@ -0,0 +1,13 @@
+/**
+ * ExpenseModal Components
+ * 
+ * Centralized exports for all modal sub-components
+ * Extracted from monolithic ExpenseSubmission.tsx
+ */
+
+export { ExpenseModalHeader } from './ExpenseModalHeader';
+export { ExpenseModalFooter } from './ExpenseModalFooter';
+export { ExpenseModalReceipt } from './ExpenseModalReceipt';
+export { ExpenseModalAuditTrail } from './ExpenseModalAuditTrail';
+export { ExpenseModalDuplicateWarning } from './ExpenseModalDuplicateWarning';
+
---
+++ src/components/expenses/ExpenseTable/index.ts
@@ -0,0 +1,10 @@
+/**
+ * ExpenseTable Components
+ * 
+ * Centralized exports for table-related components
+ * Extracted from monolithic ExpenseSubmission.tsx
+ */
+
+export { ExpenseTableFilters } from './ExpenseTableFilters';
+export { ExpenseTableRow } from './ExpenseTableRow';
+
